7a4398d794d17e4a615d3fbceb59761bb99a0722
Rgmg16 <gumborobert7@gmail.com>
2024-04-11 10:17:50 +0300

    String literals concatenation

--- String-literals-concatenation.js
+++ String-literals-concatenation.js
@@ -2,5 +2,6 @@ let user = "Dwayne";
 let str1 = "CC1";
 let str2 = "CC2";
 let str3 = "CC3";
+let str4 = `${str1}, ${str2} and ${str3}`
 
-console.log(`${user} will be doing ${str1},${str2} and ${str3}.`)
+console.log(`${user} will be doing ${str4}`)
