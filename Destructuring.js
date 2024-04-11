0217bdd8e7b65edebf0539ad21d8739a129d72a5
Rgmg16 <gumborobert7@gmail.com>
2024-04-11 12:29:51 +0300

    Destructuring in Js

--- Destructuring.js
+++ Destructuring.js
@@ -21,6 +21,21 @@ const user = {
 
 // Adding a new variable and a default value
 
-const { name, age, salary=123455 } = user;
+// const { name, age, salary=123455 } = user;
 
+// console.log(name, age, salary); 
+
+// Alternative way of doing the above
+let salary = user.salary ? user.salary : 123455;
+const { name, age,} = user;
 console.log(name, age, salary); 
+
+// 
+const user1 = { 
+    'first_name': 'Alex',
+    'last_name': 'Brandos',
+}
+const { first_name, last_name, full_name=`${first_name} ${last_name}` } = user1;
+
+console.log(full_name);
+
