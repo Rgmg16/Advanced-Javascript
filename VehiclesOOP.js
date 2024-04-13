d03bb43fcffaee0e4ac2ee3cd9b3bcd2fb188e6c
Rgmg16 <gumborobert7@gmail.com>
2024-04-13 20:24:43 +0300

    Vehicles in Object-Oriented programming

---
+++ VehiclesOOP.js
@@ -0,0 +1,28 @@
+class SUV {
+    constructor(name, phrase) {
+        this.name = name;
+        this.phrase = phrase;
+    }
+    description=
+}
+
+class Convertible {
+    constructor(name, phrase) {
+        this.name = name;
+        this.phrase = phrase;
+    }
+}
+
+class Coupe {
+    constructor(name, phrase) {
+        this.name = name;
+        this.phrase = phrase;
+    }
+}
+
+class Saloon {
+    constructor(name, phrase) {
+        this.name = name;
+        this.phrase = phrase;
+    }
+}
